d21c3c222eb4fa6b56b65832f41f52c1434145f9
abhishekzgithub <emailabhishek2@gmail.com>
2020-06-05 13:13:05 +0530

    dfs and bfs

--- graph_imp_adjacencylist.py
+++ graph_imp_adjacencylist.py
@@ -1,4 +1,6 @@
 #https://www.python-course.eu/graphs_python.php
+from collections import defaultdict
+
 class Vertex(object):
     def __init__(self,vertex):
         self.vertex=vertex
@@ -15,7 +17,11 @@ class Graph(object):
         else:
             self.vertices[vertex1].append((vertex2,weight))
     def get_vertices(self,vertex):
-        return self.vertices[vertex]
+        try:
+            vert=self.vertices[vertex]
+        except Exception as e:
+            vert=None
+        return vert
     def get_all_vertices(self):
         return self.vertices.keys()
     def add_vertex(self,vertex):
@@ -30,8 +36,6 @@ class Graph(object):
 
     def bfs(self,start):
         q = []
-        from collections import defaultdict
-        
         visited = defaultdict(lambda: False) #[False] * len(self.get_all_vertices())
         q.append(start)
         while q:
@@ -41,11 +45,52 @@ class Graph(object):
             for items in (neighbors):
                 next_node=items[0]
                 if next_node and not visited[next_node]:
-                    #print(next_node)
                     visited[next_node]=True
                     q.append(next_node)
+    def _dfs(self,start,visited):
+        print(start)
+        visited[start] = True
+        neighbors=self.get_vertices(start)
+        if not neighbors:
+            return
+        for vertex_weight in neighbors:
+            vertex=vertex_weight[0]
+            if not visited[vertex]:
+                self._dfs(vertex,visited)
+    
     def dfs(self,start):
-        pass
+        visited = defaultdict(lambda : False)
+        return self._dfs(start,visited)
+    
+    def topological_sort(self):
+        in_degree = defaultdict(lambda : 0)
+        for v in self.vertices:
+            vertices=self.get_vertices(v)
+            for vertex_edge in vertices:
+                vertex=vertex_edge[0]    
+                in_degree[vertex]+=1
+        indegree_dict={k:in_degree[k] for k in self.get_all_vertices()}
+        print("Indegree items",{str(k):in_degree[k] for k in indegree_dict.keys()})
+        result=[]
+        q=[]
+        for v,degree in indegree_dict.items():
+            if degree==0:
+                q.append(v)
+        print("initial queue items",[str(i) for i in q])
+        while q:
+            zero_indegree_vertex=q.pop()
+            result.append(zero_indegree_vertex)
+            all_nbr_vertices=self.get_vertices(zero_indegree_vertex)
+            # if not all_nbr_vertices:
+            #     return [str(i) for i in result]
+            for v_e in all_nbr_vertices:
+                v=v_e[0]
+                if indegree_dict[v]>0:
+                    indegree_dict[v]-=1
+                if indegree_dict[v]==0:
+                    q.append(v)
+
+        return [str(i) for i in result]
 
         
 A=Vertex("A")
@@ -60,17 +105,23 @@ graph.add_vertex(A)
 graph.add_vertex(A0)
 graph.add_vertex(A1)
 graph.add_vertex(A2)
-graph.add_neighbour(A,A0,10)
-graph.add_neighbour(A0,A1,1)
-graph.add_neighbour(A0,A2,12)
-#graph.add_neighbour(A1,A2,2)
-# graph.add_neighbour(A2,A3,3)
-# graph.add_neighbour(A3,A4,3)
+graph.add_vertex(A3)
+graph.add_vertex(A4)
+#graph.add_vertex(A5)
+graph.add_neighbour(A,A0,1)
+graph.add_neighbour(A0,A1,2)
+graph.add_neighbour(A0,A2,3)
+graph.add_neighbour(A1,A2,4)
+graph.add_neighbour(A2,A3,5)
+graph.add_neighbour(A2,A4,6)
+graph.add_neighbour(A3,A4,7)
 # graph.add_neighbour(A4,A5,11)
-# graph.add_neighbour(A5,A,20)
-
+#graph.add_neighbour(A5,A,20)
+#graph.add_neighbour(A4,A,11)
 #print(graph.get_vertices(A0))
 #print(graph.get_vertices(A2))
 
 print(graph)
-(graph.bfs(A))
+#graph.bfs(A)
+#print(graph.dfs(A1))
+print(graph.topological_sort())
